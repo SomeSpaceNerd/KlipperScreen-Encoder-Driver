092ecdd7a7e3d946a90d0993287c4460ebff2642
wil-sys <52981362+wil-sys@users.noreply.github.com>
2024-05-19 18:12:20 -0400

    Update EncoderMouse.py to remove unnecessary loop

--- EncoderMouse.py
+++ EncoderMouse.py
@@ -52,6 +52,5 @@ button = Button(pin_button)
 button.when_held = button_hold
 button.when_pressed = button_pressed
 
-# Keep the script running and not trip systemctl restart counter
-while True:
-    signal.pause()
+# Keep the script running
+signal.pause()
