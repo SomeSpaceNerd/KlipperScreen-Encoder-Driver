79d64c5c78da5ce86b484845ae9228c2a3e2a50f
wil-sys <52981362+wil-sys@users.noreply.github.com>
2024-05-19 18:45:11 -0400

    Updated some variables

--- EncoderMouse.py
+++ EncoderMouse.py
@@ -3,10 +3,12 @@ from evdev import UInput, ecodes as e
 import time
 import signal
 
-# Define GPIO pins
-pin_a = 27
-pin_b = 17
-pin_button = 22
+# Configurable Variables
+pin_a = 27 # Encoder A Pin
+pin_b = 17 # Encoder B Pin
+pin_button = 22 # Encoder Switch (SW) Pin
+hold_time = 0.75 # Time that holding the button to left click takes
+movement_amount = 8 # Movement multiplier for mouse movements
 
 # Mode: 0 for X-axis, 1 for Y-axis
 mode = 0
@@ -19,21 +21,21 @@ capabilities = {
 ui = UInput(capabilities)
 
 def button_hold():
+    global mode
+    mode = 1 - mode # Compensate for button_pressed() being called before button_hold()
     ui.write(e.EV_KEY, e.BTN_LEFT, 1)
     ui.syn()
     time.sleep(0.25)
     ui.write(e.EV_KEY, e.BTN_LEFT, 0)
     ui.syn()
-    mode = 1 - mode # Compensate for button_pressed() being called before button_hold()
 
 def button_pressed():
     global mode
     mode = 1 - mode  # Toggle between 0 and 1
 
 def move_mouse():
-    global encoder
+    global encoder, movement_amount
     steps = encoder.steps
-    movement_amount = 8  # Adjust the movement multiplier here
     if mode == 0:
         if steps > 0:
             ui.write(e.EV_REL, e.REL_X, -movement_amount)
@@ -52,7 +54,7 @@ encoder = RotaryEncoder(pin_a, pin_b)
 encoder.when_rotated = move_mouse
 
 button = Button(pin_button)
-button.hold_time = 0.75
+button.hold_time = hold_time
 button.when_held = button_hold
 button.when_pressed = button_pressed
 
